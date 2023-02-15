a4d7a6c7ddebff5b428109c227cce8f3d903d5d2
Eldar Temirov <temirov.e.i.21@gmail.com>
2023-02-15 09:02:15 +0300

    Работа с атрибутами в фреймворке Vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
-<script>
-export default {
-  data() {
-    return {
-      name: "Eldar",
-      surn: "Temirov",
-    };
-  },
-};
-</script>
-
 <template>
-  <p>{{ surn }}</p>
-  <p>{{ name }}</p>
-</template>
+	<div class="body">
+	  <div class="main">
+		<a :href="page">KIPU</a>
+	  </div>
+	</div>
+  </template>
+  
+  <script>
+  export default {
+	data() {
+	  return {
+		page: "page.html",
+	  };
+	},
+  };
+  </script>
