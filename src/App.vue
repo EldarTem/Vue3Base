9d36dd66a77f2410ee063e73e55611d733a07ee5
Eldar Temirov <temirov.e.i.21@gmail.com>
2023-02-15 09:21:25 +0300

    Вспомогательные методы в Vue

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,47 @@
 <template>
-	<div class="body">
-	  <div class="main">
-		<button @click="show()">On click</button>
-	  </div>
+
+	<div id='cmd_head'>
+	<p>Console</p>
 	</div>
+  
+	<div id='cmd'>
+	  <p>> Hello!</p>
+	</div>
+	{{ show() }}
+	<!-- <button @click="show">OK</button> -->
   </template>
   
   <script>
+  let arr = prompt('Введите чсило от 1 до 7!')
+  let week =[
+		'Понедельник',
+		'Вторник',
+		'Среда',
+		'Четверг',
+		'Пятница',
+		'Суббота',
+		'Воскресенье'
+  ];
   export default {
-	data() {
+	  data() {
 	  return {
-		num1: 1,
-		num2: 2,
-	  };
+		days: week,
+		param: Number(arr),
+		}
 	},
 	methods: {
-	  show: function () {
-		alert(this.num1 + this.num2);
-	  },
-	},
-  };
+		show: function() {
+			let day = this.cape(this.param);
+		let now = new Date();
+		alert("Ваш день недели: " + day);
+		alert("Cегодняшний день недели: " + week[now.getDay() - 1]);
+		},
+	  cape: function(num) {
+		if (num > 7 || num < 1){
+		  return 'Число не входит в диапазон от 1 до 7';
+		}
+		return this.days[num-1];    
+	  }
+	}
+  }
   </script>
