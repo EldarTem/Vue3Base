e03511cdfd4148c0fe651d201bb276906d9533ee
Eldar Temirov <temirov.e.i.21@gmail.com>
2023-02-22 23:06:29 +0300

    Работа с радиокнопками в Vue

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,39 @@
 export default {
   data() {
     return {
-      arr: [],
+      language: '',
+      visible: false,
+      visible1: false,
+      visible2: false,
+    }
+  },
+  methods: {
+    one() {
+      this.visible = true;
+    },
+    two() {
+      this.visible1 = true;
+    },
+    three() {
+      this.visible2 = true;
     }
   }
 }
 </script>
 <template>
-  <input type="checkbox" v-model="arr" value="Русский">
-  <label>Русский</label>
-  <br>
-  <input type="checkbox" v-model="arr" value="Английский">
-  <label>Английский</label>
-  <br>
-  <input type="checkbox" v-model="arr" value="Немецкий">
-  <label>Немецкий</label>
-  <ul>
-  <li v-for="elem in arr">{{ elem }}</li>
-  </ul>
+      <input type="radio" v-model="language" value="Русский" @click="first">
+      <p>Russian</p>
+      <p></p>
+      <input type="radio" v-model="language" value="Английский" @click="second">
+      <p>UK</p>
+      <p></p>
+      <input type="radio" v-model="language" value="Немецкий" @click="third">
+      <p></p>
+      <p>Germany</p>
+      <p v-if="visible">Привет</p>
+      <p v-else-if="visible1">Hello</p>
+      <p v-else-if="visible2">Gutten tack</p>
+
 </template>
 <style>
 
