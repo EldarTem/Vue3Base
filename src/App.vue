07b0e3d574b48c96ffb50b7c833623545716504f
Eldar Temirov <temirov.e.i.21@gmail.com>
2023-03-01 12:22:23 +0300

    Передача данных любого типа в Vue

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,12 @@ import Employee from './components/Employee.vue'
 export default {
   components: {
     Employee
-  },
-  data() {
-    return {
-    }
-  },
-  methods: {
   }
 }
 </script>
 
 <template>
-  <Employee name="Эльдар" surn="Темиров" age="19" />
+  <Employee :arg="{ a: 1, b: 2, c: 3 }" />
 </template>
 
 <style>
